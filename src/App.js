71f4d67a063b820998e8f51da9b24fd5395978a3
Srinath Vasudevan <svasude5@ncsu.edu>
2022-04-02 18:22:10 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import DirectedMap from './DirectedMap';
+import { withScriptjs } from "react-google-maps";
+require('dotenv').config()
+
+
+
+// function App() {
+//   return (
+//     <div dangerouslySetInnerHTML={{ __html: `<iframe
+//     width="700"
+//     height="700"
+//     frameborder="0" style="border:0"
+//     referrerpolicy="no-referrer-when-downgrade"
+//     src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyCCjkUAUMwYMmgeYQmgSH7VjU8kjSHztr4&origin=120+Folsom+Drive,+Holly+Springs,+NC&destination=Rovisys+Holly+Springs"
+//     allowfullscreen>
+//     </iframe>`}} />
+//   );
+// }
 
 function App() {
+
+  const MapLoader = withScriptjs(DirectedMap);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<div className="App">
+  <MapLoader 
+      origin="EB III, Raleigh, NC"
+      destination="Valentine Commons, Raleigh, NC"
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCCjkUAUMwYMmgeYQmgSH7VjU8kjSHztr4"
+      loadingElement={<div style={{ height: `100%` }} />}
+  />
+
+</div>
   );
 }
 
