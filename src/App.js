84a350dc2b5d789f9d792352e90f979d0c43b453
Srinath Vasudevan <svasude5@ncsu.edu>
2022-04-02 22:23:35 -0400

    bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import "./components/ClassInput/ClassInput";
 import ClassInput from "./components/ClassInput/ClassInput";
 import Map from "./components/Map/Map"
 import { API_KEY } from './private';
+import {Row, Col} from "react-bootstrap";
 
 function App(props) {
   
   return (
   <div>
   <ClassInput/>
-  <Map origin='120 Folsom Drive, Holly Springs, NC' destination='Rovisys, Holly Springs, NC' />
+  <Row>
+    <Col>
+    <Map origin='120 Folsom Drive, Holly Springs, NC' destination='NCSU' mode='bicycling'/>
+    </Col>
+    <Col>
+    <Map origin='120 Folsom Drive, Holly Springs, NC' destination='NCSU' mode='walking'/>
+    </Col>
+    <Col>
+    <Map origin='120 Folsom Drive, Holly Springs, NC' destination='NCSU' mode='transit'/>
+    </Col>
+  </Row>
   </div>
   );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,38 +4,41 @@ import { API_KEY } from "../../private";
 function getMap(origin, destination)  {
     var originMod = origin.replaceAll(" ","+");
     var destinationMod = destination.replaceAll(" ", "+");
-    var bike = fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${API_KEY}&origin=${originMod}&destination=${destinationMod}&mode=bicycling`)
+    var bike = 0;
+    var walk = 0;
+    var transit = 0;
+    fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${API_KEY}&origin=${originMod}&destination=${destinationMod}&mode=bicycling`,{mode: 'no-cors'})
     .then(response => response.json())
     .then(data => {
-        return data.routes[0].legs[0].value;
+        bike = data.routes[0].legs[0].value;
+        fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${API_KEY}&origin=${originMod}&destination=${destinationMod}&mode=walking`,{mode: 'no-cors'})
+        .then(response => response.json())
+        .then(data => {
+            walk =  data.routes[0].legs[0].value;
+            fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${API_KEY}&origin=${originMod}&destination=${destinationMod}&mode=transit`,{mode: 'no-cors'})
+            .then(response => response.json())
+            .then(data => {
+                transit = data.routes[0].legs[0].value;
+                if (bike < walk && bike < transit) {
+                    return 'bicycling';
+                }
+            
+                if (walk < bike && walk < transit) {
+                    return 'walking';
+                }
+            
+                if (transit < walk && transit < bike) {
+                    return 'transit';
+                }
+            }
+            );
+        }
+        );
     }
     );
     
-    var walk = fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${API_KEY}&origin=${originMod}&destination=${destinationMod}&mode=walking`)
-    .then(response => response.json())
-    .then(data => {
-        return data.routes[0].legs[0].value;
-    }
-    );
 
-    var transit = walk = fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${API_KEY}&origin=${originMod}&destination=${destinationMod}&mode=transit`)
-    .then(response => response.json())
-    .then(data => {
-        return data.routes[0].legs[0].value;
-    }
-    );
-
-    if (bike < walk && bike < transit) {
-        return 'bicycling';
-    }
-
-    if (walk < bike && walk < transit) {
-        return 'walking';
-    }
-
-    if (transit < walk && transit < bike) {
-        return 'transit';
-    }
+    
 
     
   }
@@ -43,11 +46,11 @@ function getMap(origin, destination)  {
 const Map = (props) => {
     const [origin, setOrigin] = useState('');
     const [dest, setDest] = useState('');
-
+    console.log(getMap(props.origin, props.destination))
     return (
         <div dangerouslySetInnerHTML={{ __html: '<iframe '.concat(
-            'width="700" ',
-            'height="700" ',
+            'width="400" ',
+            'height="400" ',
             'frameborder="0" style="border:0" ',
             'referrerpolicy="no-referrer-when-downgrade" ',
             'src="https://www.google.com/maps/embed/v1/directions?key=',
@@ -56,6 +59,8 @@ const Map = (props) => {
             props.origin.replaceAll(" ", "+"),
             '&destination=',
             props.destination.replaceAll(" ", "+"),
+            '&mode=',
+            props.mode,
             '" allowfullscreen> ',
             '</iframe>')}} />
       );
